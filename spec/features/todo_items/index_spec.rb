e4c1fd32d8b4a0b8241aea70f5e6e58d8a9323af
LeJaques <michal.lubczynski@gmail.com>
2015-07-05 02:09:27 +0200

    Added test for creating todo items and associated code

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -4,13 +4,25 @@ describe "Viewing todo items" do
 
   let!(:todo_list) {TodoList.create(title: "Grocery list", description: "Groceries") }             
 
-  it "displays no items when a todo_list is empty" do
-    visit "/todo_lists"
-    within "#todo_list_#{todo_list.id}" do
+  def visit_todo_list (list)
+      visit "/todo_lists"
+      within "#todo_list_#{list.id}" do
       click_link "List Items"
     end
+  end
+  
+  before do 
+   
+  end
 
-    expect(page).to have_content("TodoItems#index")
+  it "displays the title of the todo list" do
+    visit_todo_list(todo_list)
+    within ("h1") do 
+      expect(page).to have_content(todo_list.title)
+    end
+  end
+  it "displays no items when a todo_list is empty" do
+    expect(page.all("ul.todo_items li").size).to eq(0)
 
   end
 end
