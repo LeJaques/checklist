cc657bc340c5cc832648cb9ecd96f2a4cc67a62b
LeJaques <michal.lubczynski@gmail.com>
2015-07-07 00:02:12 +0200

    Added specs and code for editing todo items

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,9 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+
+  validates :content, presence: true,
+                      length: { minimum: 3 }
+
+
+
 end
--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -17,9 +17,30 @@ describe "Adding todo items" do
     fill_in "Content", with: "Milk"
     click_button "Save"
     expect(page).to have_content("Added todo list item.")
-    within("ul.todo.items") do 
+    within "ul.todo_items li" do 
       expect(page).to have_content("Milk")
     end 
   end
 
+  it "displays an error with no content" do
+   visit_todo_list(todo_list)
+   click_link "New Todo Item"
+   fill_in "Content", with: ""
+   click_button "Save"
+   within("div.flash") do
+     expect(page).to have_content("An error occurred while adding that todo list item.")
+    end
+   expect(page).to have_content("Content can't be blank")
+  end
+
+  it "displays an error with content less than 2 characters long" do
+   visit_todo_list(todo_list)
+   click_link "New Todo Item"
+   fill_in "Content", with: "1"
+   click_button "Save"
+   within("div.flash") do
+     expect(page).to have_content("An error occurred while adding that todo list item.")
+    end
+    expect(page).to have_content("Content is too short")
+  end
 end
